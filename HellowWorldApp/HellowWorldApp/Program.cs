f6ad73455963ca622b0ab8912d69bb9a4700511e
benazir1853 <jahan.058@gmail.com>
2014-12-10 15:42:24 -0800

    New file Added

---
+++ HellowWorldApp/HellowWorldApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HellowWorldApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double i = 9;
+            double result = GetSquare(i);
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+        
+        private static double GetSquare(double aValue)
+        {
+
+            return aValue*aValue;
+        
+        }
+    }
+}
---
+++ MakeDoubleApp2/WindowsFormsApplication3/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double aNumber = Convert.ToDouble(inputValueTextBox.Text);
+            double result = MakeDouble(aNumber);
+            doubleValueTextBox.Text = result.ToString();
+        }
+
+        private double MakeDouble(double input)
+        {
+            return input * 2;
+        }
+
+
+    }
+}
